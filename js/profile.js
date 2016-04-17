78f52cce8de06bd02e4832eda01891e85adb6fa8
ShaunApps <shaun.apps@gmail.com>
2016-04-16 23:48:35 -0400

    imported initial react setup from codepen

---
+++ js/profile.js
@@ -0,0 +1,16 @@
+class Profile extends React.Component {
+  render() {
+    var { name, age, bio, pic} = this.props;
+    return (
+      <div className="profile-box">
+        <h2>{name}</h2>
+        <h4>Age: {age}</h4>
+        <h4>Bio: {bio}</h4>
+        <img src={pic} height={100} />
+      </div>
+    );
+  }
+
+}
+// props
+React.render(<Profile name="Shaun Appelbaum" age={27} bio="I write code and lift weights." pic="" />, document.getElementById('app'));
