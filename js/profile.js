1299e4a10ae0b71da7939406e2d27bc496873ecc
ShaunApps <shaun.apps@gmail.com>
2016-04-21 17:37:21 -0400

    added a ProjectsContainer component

--- js/profile.js
+++ js/profile.js
@@ -8,11 +8,28 @@ class Profile extends React.Component {
         <h2>{name}</h2>
         <h4>Age: {age}</h4>
         <h4>Bio: {bio}</h4>
-        <img src={pic} height={100} />
       </div>
     );
   }
 
 }
+
+
+
+class ProjectsContainer extends React.Component {
+  render() {
+    var { name, image, link } = this.props;
+    return (
+      <div>
+        <h3>{name}</h2>
+        <a href={link}>
+          <img src={image}>
+        </a>
+      </div>
+    );
+  }
+}
 // props
-React.render(<Profile name="Shaun Appelbaum" age={27} bio="I write code and lift weights." pic="" />, document.getElementById('app'));
+React.render(<Profile name="Shaun Appelbaum" age={27} bio="I write code and lift weights."/>, document.getElementById('app'))
+React.render(<ProjectsContainer name="Couplr" image="placeholder" link="http://www.couplrapp.co/"/>, document.getElementById('app'));
+;
