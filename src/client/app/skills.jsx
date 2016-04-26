4beee08ec1a43de18295b3d55e735c2fe8fdfdb9
ShaunApps <shaun.apps@gmail.com>
2016-04-25 21:34:35 -0400

    added to the skillset sections functions

--- src/client/app/skills.jsx
+++ src/client/app/skills.jsx
@@ -7,4 +7,44 @@ const = TECH_USED = [
   { name: 'React.js', image: ''},
   { name: 'Ruby', image: ''},
   { name: 'Rails', image: ''}
+  { name: 'HTML', image: ''},
+  { name: 'CSS', image: ''}
 ]
+
+class SkillSetBox extends React.Component {
+  render() {
+    var rows = [];
+    this.props.skills.forEach(function(skill) {
+      rows.push(<SkillSetRow skill={skill} key={skill.name} />);
+};
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Skill</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+
+    );
+  }
+}
+
+
+class SkillSetRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td colSpan="4">{this.props.skill.name}</td>
+        <td colSpan="4">{this.props.skill.image}</td>
+      </tr>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <SkillSetBox skills={TECH_USED} />,
+  document.getElementById('skillset')
+);
